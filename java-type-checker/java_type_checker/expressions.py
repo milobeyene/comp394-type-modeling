478ee7f1b6fb7b8dc90878dd2205db19586bd12f
milobeyene <mbeyene@macalester.edu>
2018-02-11 20:25:57 -0600

    Finished assignment

--- java-type-checker/java_type_checker/expressions.py
+++ java-type-checker/java_type_checker/expressions.py
@@ -5,73 +5,175 @@ from .types import Type
 
 class Expression(object):
     """
-    AST for simple Java expressions. Note that this package deal only with compile-time types;
-    this class does not actually _evaluate_ expressions.
-    """
-
+        AST for simple Java expressions. Note that this package deal only with compile-time types;
+        this class does not actually _evaluate_ expressions.
+        """
+    
     def static_type(self):
         """
-        Returns the compile-time type of this expression, i.e. the most specific type that describes
-        all the possible values it could take on at runtime. Subclasses must implement this method.
-        """
-        raise NotImplementedError(type(self).__name__ + " must implement static_type()")
-
+            Returns the compile-time type of this expression, i.e. the most specific type that describes
+            all the possible values it could take on at runtime. Subclasses must implement this method.
+            """
+        pass
+    
     def check_types(self):
         """
-        Validates the structure of this expression, checking for any logical inconsistencies in the
-        child nodes and the operation this expression applies to them.
-        """
-        raise NotImplementedError(type(self).__name__ + " must implement check_types()")
+            Validates the structure of this expression, checking for any logical inconsistencies in the
+            child nodes and the operation this expression applies to them.
+            """
+        
+        pass
 
 
 class Variable(Expression):
     """ An expression that reads the value of a variable, e.g. `x` in the expression `x + 5`.
-    """
+        """
     def __init__(self, name, declared_type):
         self.name = name                    #: The name of the variable
         self.declared_type = declared_type  #: The declared type of the variable (Type)
+    
+    def static_type(self):
+        return self.declared_type
+    
+    def check_types(self):
+        pass
 
 
 class Literal(Expression):
     """ A literal value entered in the code, e.g. `5` in the expression `x + 5`.
-    """
+        """
     def __init__(self, value, type):
         self.value = value  #: The literal value, as a string
         self.type = type    #: The type of the literal (Type)
-
+    
+    def static_type(self):
+        return self.type
+    
+    def check_types(self):
+        pass
 
 class NullLiteral(Literal):
     def __init__(self):
         super().__init__("null", Type.null)
+    
+    def static_type(self):
+        return Type.null
+
 
 
 class MethodCall(Expression):
     """
-    A Java method invocation, i.e. `foo.bar(0, 1, 2)`.
-    """
+        A Java method invocation, i.e. `foo.bar(0, 1, 2)`.
+        """
     def __init__(self, receiver, method_name, *args):
-        self.receiver = receiver
         self.receiver = receiver        #: The object whose method we are calling (Expression)
         self.method_name = method_name  #: The name of the method to call (String)
         self.args = args                #: The method arguments (list of Expressions)
+    
+    def static_type(self):
+        objectType = self.receiver.static_type()
+        method = objectType.method_named(self.method_name)
+        return method.return_type
+    
+    def check_types(self):
+        primitives = [Type.int, Type.boolean, Type.double]
+        objectType = self.receiver.static_type()
+        passedTypes = []
+        
+        if objectType in primitives:
+            raise JavaTypeError("Type {0} does not have methods".format(
+                                                                        objectType.name
+                                                                        ))
+        
+        if objectType.method_named(self.method_name):
+            method = objectType.method_named(self.method_name)
+            pass
+        else:
+            raise JavaTypeError("{0} has no method named {1}".format(
+                                                                     objectType.name,
+                                                                     self.method_name
+                                                                     ))
+        
+        if len(self.args) == len(method.argument_types):
+            pass
+        else:
+            raise JavaTypeError("Wrong number of arguments for {0}: expected {1}, got {2}".format(
+                                                                                                  objectType.name + "." + method.name+ "()",
+                                                                                                  len(method.argument_types),
+                                                                                                  len(self.args)))
+
+        for e in self.args:
+            passedTypes.append(e.static_type())
+            for i in range(0,len(self.args)):
+self.args[i].check_types()
+    if passedTypes[i] == method.argument_types[i]:
+        pass
+            elif method.argument_types[i] in passedTypes[i].direct_supertypes or passedTypes[i] == Type.null:
+                pass
+            else:
+                raise JavaTypeError("{0} expects arguments of type {1}, but got {2}".format(
+                                                                                            objectType.name + "." + method.name + "()",
+                                                                                            names(method.argument_types),
+                                                                                            names(passedArgTypes)))
 
 
 class ConstructorCall(Expression):
     """
-    A Java object instantiation, i.e. `new Foo(0, 1, 2)`.
-    """
+        A Java object instantiation, i.e. `new Foo(0, 1, 2)`.
+        """
     def __init__(self, instantiated_type, *args):
         self.instantiated_type = instantiated_type  #: The type to instantiate (Type)
         self.args = args                            #: Constructor arguments (list of Expressions)
+    
+    def static_type(self):
+        return self.instantiated_type
+    
+    def check_types(self):
+        # can't instantiate primitives
+        primitives = [Type.int, Type.boolean, Type.double]
+        if self.instantiated_type in primitives:
+            raise JavaTypeError("Type {0} is not instantiable".format(
+                                                                      self.instantiated_type.name
+                                                                      ))
+        if self.instantiated_type == Type.null:
+            raise JavaTypeError("Type null is not instantiable")
+        
+        passedTypes = []     #setup a list for the types of passed arguments
+        expectedTypes = self.instantiated_type.constructor.argument_types
+        # make sure you passed the expected number of arguments
+        if len(self.args) == len(expectedTypes):
+            pass
+        else:
+            raise JavaTypeError("Wrong number of arguments for {0}: expected {1}, got {2}".format(
+                                                                                                  self.instantiated_type.name + " constructor",
+                                                                                                  len(expectedTypes),
+                                                                                                  len(self.args)))
+        
+        # make sure you passed arguments with the expected types (or supertypes)
+        for e in self.args:
+            passedTypes.append(e.static_type())
+            for i in range(0,len(self.args)):
+            self.args[i].check_types()
+            if passedTypes[i] == expectedTypes[i]:
+                pass
+            elif expectedTypes[i] in passedTypes[i].direct_supertypes:
+                pass
+elif passedTypes[i] == Type.null and not expectedTypes[i] in primitives:
+    pass
+        else:
+            raise JavaTypeError("{0} expects arguments of type {1}, but got {2}".format(
+                                                                                        self.instantiated_type.name + " constructor",
+                                                                                        names(expectedTypes),
+                                                                                        names(passedTypes)))
 
 
 class JavaTypeError(Exception):
     """ Indicates a compile-time type error in an expression.
-    """
+        """
     pass
 
 
 def names(named_things):
     """ Helper for formatting pretty error messages
-    """
+        """
     return "(" + ", ".join([e.name for e in named_things]) + ")"
--- java-type-checker/java_type_checker/types.py
+++ java-type-checker/java_type_checker/types.py
@@ -3,33 +3,41 @@
 
 class Type(object):
     """ Represents any Java type, including both class types and primitives.
-    """
+        """
     def __init__(self, name, direct_supertypes=[]):
         self.name = name
         self.direct_supertypes = direct_supertypes
         self.is_instantiable = False
-
+    
     def is_subtype_of(self, other):
         """ True if this type can be used where the other type is expected.
-        """
-        return True  # TODO: implement
+            """
+        if self.name == other.name:
+            return True
+        for supertype in self.direct_supertypes:
+            if supertype.name == other.name:
+                return True
+            for superSupertype in supertype.direct_supertypes:
+                if superSupertype.name == other.name:
+                    return True
+        return False
 
-    def is_supertype_of(self, other):
-        """ Convenience counterpart to is_subtype_of().
+def is_supertype_of(self, other):
+    """ Convenience counterpart to is_subtype_of().
         """
-        return other.is_subtype_of(self)
+            return other.is_subtype_of(self)
 
 
 class Constructor(object):
     """ The declaration of a Java constructor.
-    """
+        """
     def __init__(self, argument_types=[]):
         self.argument_types = argument_types
 
 
 class Method(object):
     """ The declaration of a Java method.
-    """
+        """
     def __init__(self, name, argument_types=[], return_type=None):
         self.name = name
         self.argument_types = argument_types
@@ -38,23 +46,22 @@ class Method(object):
 
 class ClassOrInterface(Type):
     """
-    Describes the API of a class-like Java type (class or interface).
-
-    (This type model does not draw a distinction between classes and interfaces,
-    and assumes they are all instantiable. Other than instantiability, the
-    distinction makes no difference to us here: we are only checking types, not
-    compiling or executing code, so none of the methods have implementations.)
-    """
+        Describes the API of a class-like Java type (class or interface).
+        (This type model does not draw a distinction between classes and interfaces,
+        and assumes they are all instantiable. Other than instantiability, the
+        distinction makes no difference to us here: we are only checking types, not
+        compiling or executing code, so none of the methods have implementations.)
+        """
     def __init__(self, name, direct_supertypes=[], constructor=Constructor([]), methods=[]):
         super().__init__(name, direct_supertypes)
         self.name = name
         self.constructor = constructor
         self.methods = {method.name: method for method in methods}
         self.is_instantiable = True
-
+    
     def method_named(self, name):
         """ Returns the Method with the given name, which may come from a supertype.
-        """
+            """
         try:
             return self.methods[name]
         except KeyError:
@@ -68,9 +75,16 @@ class ClassOrInterface(Type):
 
 class NullType(Type):
     """ The type of the value `null` in Java.
-    """
+        """
     def __init__(self):
         super().__init__("null")
+    
+    def is_subtype_of(self, other):
+        return True
+    
+    def method_named(self, name):
+        raise NoSuchMethod("Cannot invoke method {0} on null".format(
+                                                                     name + "()"))
 
 
 class NoSuchMethod(Exception):
@@ -88,7 +102,7 @@ Type.double  = Type("double")
 Type.null    = NullType()
 
 Type.object = ClassOrInterface("Object",
-    methods=[
-        Method("equals", argument_types=[object], return_type=Type.boolean),
-        Method("hashCode", return_type=Type.int),
-    ])
+                               methods=[
+                                        Method("equals", argument_types=[object], return_type=Type.boolean),
+                                        Method("hashCode", return_type=Type.int),
+                                        ])
